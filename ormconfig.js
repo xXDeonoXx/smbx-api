81a2d4d912cf0484aa3fb8a3860a3db7a3093376
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2020-12-27 13:10:38 -0300

    feat: configure ORM settings and add important libs

--- ormconfig.js
+++ ormconfig.js
@@ -8,38 +8,13 @@ module.exports = [
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    migrations: [
-      `./${isDevelopment ? 'src' : 'dist'}/database/migrations/*{.ts,.js}`,
-    ],
+    entities: ['**/*.entity{.ts,.js}'],
+
+    migrationsTableName: 'migrations',
+
+    migrations: [`src/database/migrations/*.ts`],
     cli: {
-      migrationsDir: `./${isDevelopment ? 'src' : 'dist'}/database/migrations`,
+      migrationsDir: 'database/migrations',
     },
-    entities: [`./${isDevelopment ? 'src' : 'dist'}/models/**/*{.ts,.js}`],
-    logging: process.env.DB_LOGGING_LEVEL.split(','),
-  },
-  {
-    name: 'seed',
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    migrations: [
-      `./${isDevelopment ? 'src' : 'dist'}/database/seeds/*{.ts,.js}`,
-    ],
-    cli: {
-      migrationsDir: `./${isDevelopment ? 'src' : 'dist'}/database/seeds`,
-    },
-    entities: [`./${isDevelopment ? 'src' : 'dist'}/models/**/*{.ts,.js}`],
-    logging: process.env.DB_LOGGING_LEVEL.split(','),
-  },
-  {
-    name: 'test',
-    type: 'sqlite',
-    database: 'test.db',
-    dropSchema: true,
-    entities: ['./src/models/**/*.ts'],
-    synchronize: true,
   },
 ];
